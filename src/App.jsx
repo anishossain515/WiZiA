1cde56e62b987735ea5cacc0b7c03dc7c1e99476
anishossain515 <amirul447700@gmail.com>
2024-11-26 23:49:17 +0600

    Partners Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './Components/Hero';
 import NavbarArea from './Components/Navbar';
+import Partners from './Components/Partners';
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavbarArea/>
       <div>
         <Hero/>
+        <Partners/>
       </div>
     </main>
   )
---
+++ src/Components/Partners.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Partners() {
+  return (
+    <section className='bg-[#07292F] '>
+
+        <div className='max-w-[800px] mx-auto py-10'>
+
+            <p className='text-Aqua font-Montserrat text-center'>our trusted partners</p>
+            <div className='flex flex-wrap items-center justify-center md:gap-8 sm:gap-4 gap-2 px-2'>
+                <img src="/BackHub.png" alt="BackHub" />
+                <img src="/CableLabs.png" alt="CableLabs" />
+                <img src="/DBS.png" alt="DBS" />
+                <img src="/EasyEuro.png" alt="EasyEuro" />
+                <img src="/AMD.png" alt="AMD" />
+            </div>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Partners
