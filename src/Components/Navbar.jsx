66a5574fa7aebd46b97a682d020e05fb7ee32dc5
anishossain515 <amirul447700@gmail.com>
2024-11-26 21:50:21 +0600

    Navbar Section Done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,14 +11,17 @@ import {
   NavbarMenuItem,
 } from "@nextui-org/react";
 function NavbarArea() {
-  const [isMenuOpen,setIsMenuOpen] = React.useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const NavbarItems =['About Us','Pricing','Customers','Solutions']
+  const NavbarItems = ["About Us", "Pricing", "Customers", "Solutions"];
 
   return (
-    <Navbar isBlurred={true}  className="w-full bg-transparent" maxWidth="xl" onMenuOpenChange={setIsMenuOpen}>
-
-
+    <Navbar
+      isBlurred={true}
+      className="w-full bg-transparent"
+      maxWidth="xl"
+      onMenuOpenChange={setIsMenuOpen}
+    >
       {/* NavbarLogo */}
       <NavbarBrand justify="start">
         <img src="/Logo.png" alt="logo" />
@@ -26,50 +29,63 @@ function NavbarArea() {
 
       {/* NavbarList */}
       <NavbarContent className="text-white hidden md:flex">
-     
-          {NavbarItems.map((item,index)=>(
-            <NavbarItem key={index}>{item}</NavbarItem>
-          ))}
-
+        {NavbarItems.map((item, index) => (
+          <NavbarItem key={index}>{item}</NavbarItem>
+        ))}
       </NavbarContent>
 
       {/* NavbarButton */}
       <NavbarContent justify="end">
-         
         {/* Toggle button  */}
-      <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} className="text-white md:hidden" />
-         {/* Toggle button  */}
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="text-white md:hidden"
+        />
+        {/* Toggle button  */}
 
         <NavbarItem className="md:flex hidden">
-          <Button as={Link} className="bg-Aqua text-dark_teal font-medium md:flex hidden rounded-[100px]  shadow-glow" >Book a Demo</Button>
-        </NavbarItem>
-
-        <NavbarItem  className="md:flex hidden">
-          <Button as={Link} variant="bordered" className="rounded-[100px] shadow-glow text-white font-medium">
-            Contact Us
+          <Button
+            as={Link}
+            className="bg-Aqua text-dark_teal font-medium md:flex hidden rounded-[100px]  shadow-glow"
+          >
+            Book a Demo
           </Button>
         </NavbarItem>
 
+        <NavbarItem className="md:flex hidden">
+          <Button
+            as={Link}
+            variant="bordered"
+            className="rounded-[100px] shadow-glow text-white font-medium"
+          >
+            Contact Us
+          </Button>
+        </NavbarItem>
       </NavbarContent>
-     
-     {/*MenuBar*/}
+
+      {/*MenuBar*/}
       <NavbarMenu>
-         {NavbarItems.map((item,index)=>(
-          <NavbarMenuItem key={index}>
-             {item}
-          </NavbarMenuItem>
-         ))}
-         <NavbarMenuItem className="space-x-4 pt-2">
-         <Button as={Link} variant="bordered" className="rounded-[100px] shadow-glow text-white font-medium">Contact Us</Button>
-         <Button as={Link} className="bg-Aqua text-dark_teal font-medium rounded-[100px]  shadow-glow" >Book a Demo</Button>
+        {NavbarItems.map((item, index) => (
+          <NavbarMenuItem key={index}>{item}</NavbarMenuItem>
+        ))}
+        <NavbarMenuItem className="space-x-4 pt-2">
+          <Button
+            as={Link}
+            variant="bordered"
+            className="rounded-[100px] shadow-glow text-white font-medium"
+          >
+            Contact Us
+          </Button>
+          <Button
+            as={Link}
+            className="bg-Aqua text-dark_teal font-medium rounded-[100px]  shadow-glow"
+          >
+            Book a Demo
+          </Button>
         </NavbarMenuItem>
       </NavbarMenu>
-
     </Navbar>
   );
 }
 
 export default NavbarArea;
-
-
-
