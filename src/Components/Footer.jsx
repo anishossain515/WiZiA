fe893a7295857382aa2f732b33f92286171d2e76
anishossain515 <amirul447700@gmail.com>
2024-11-29 00:22:45 +0600

    Footer style update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-[#07292F]">
       <div className="pt-10 pb-6 md:px-[120px] sm:px-20 px-10 flex sm:flex-row flex-col gap-6 justify-between items-center">
         <img src="/footerLogo.png" alt="footerLogo" />
-        <p className="text-[#96ACAF] font-light">
+        <p className="text-[#96ACAF] sm:text-base text-sm font-light">
           Copyright © 2023 Wizia. All rights reserved.
         </p>
       </div>
