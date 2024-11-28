91fc8a5a629222e3727be3a8562aa8e7d6952383
anishossain515 <amirul447700@gmail.com>
2024-11-29 00:04:50 +0600

    Navbar Menu problem solve

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -35,12 +35,12 @@ function NavbarArea() {
       {/* NavbarList */}
       <NavbarContent className="text-white hidden md:flex cursor-pointer">
         {NavbarItems.map((item, index) => (
-            <NavbarItem key={index}>
-              <Link to={item.path} className="text-white">
-                {item.name}
-              </Link>
-            </NavbarItem>
-          ))}
+          <NavbarItem key={index}>
+            <Link to={item.path} className="text-white">
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
 
       {/* NavbarButton */}
@@ -53,16 +53,13 @@ function NavbarArea() {
         {/* Toggle button  */}
 
         <NavbarItem className="md:flex hidden">
-          <Button
-            className="bg-Aqua text-dark_teal font-medium md:flex hidden rounded-[100px]  shadow-glow"
-          >
+          <Button className="bg-Aqua text-dark_teal font-medium md:flex hidden rounded-[100px]  shadow-glow">
             Book a Demo
           </Button>
         </NavbarItem>
 
         <NavbarItem className="md:flex hidden">
           <Button
-        
             variant="bordered"
             className="rounded-[100px] shadow-glow text-white font-medium"
           >
@@ -72,9 +69,13 @@ function NavbarArea() {
       </NavbarContent>
 
       {/*MenuBar*/}
-      <NavbarMenu className='bg-[#063740]'>
+      <NavbarMenu className="bg-[#063740]">
         {NavbarItems.map((item, index) => (
-          <NavbarMenuItem key={index} className="text-white">{item}</NavbarMenuItem>
+          <NavbarMenuItem key={index}>
+            <Link to={item.path} className="text-white">
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
         ))}
         <NavbarMenuItem className="space-x-4 pt-2">
           <Button
@@ -83,9 +84,7 @@ function NavbarArea() {
           >
             Contact Us
           </Button>
-          <Button
-            className="bg-Aqua text-dark_teal font-medium rounded-[100px]  shadow-glow"
-          >
+          <Button className="bg-Aqua text-dark_teal font-medium rounded-[100px]  shadow-glow">
             Book a Demo
           </Button>
         </NavbarMenuItem>
