c5ddd738bff97c6170651f649409fefa03ed459b
anishossain515 <amirul447700@gmail.com>
2024-11-29 00:07:48 +0600

    UpComing Section Update style

--- src/Components/UpComing.jsx
+++ src/Components/UpComing.jsx
@@ -8,8 +8,8 @@ function UpComing() {
     Navigate("/");
   };
   return (
-    <div className="text-center py-4 text-white flex items-center justify-center gap-10">
-      <h2 className=" text-5xl"> Coming Soon...</h2>
+    <div className="text-center py-4 text-white flex sm:flex-row flex-col items-center justify-center gap-10">
+      <h2 className=" sm:text-5xl text-4xl"> Coming Soon...</h2>
       <button
         onClick={BackPage}
         className="bg-Aqua mt-2 py-3 px-[25px] rounded-lg text-dark_teal "
