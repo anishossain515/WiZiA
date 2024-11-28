868d9c35e0f3159e7f96d810b2c4dbd3b5a910d7
anishossain515 <amirul447700@gmail.com>
2024-11-28 22:56:08 +0600

    Rest Problem solve

--- src/Components/Rest.jsx
+++ src/Components/Rest.jsx
@@ -12,7 +12,7 @@ function Rest() {
          <h3 className='text-Aqua font-Montserrat font-medium italic md:text-[32px] sm:text-2xl text-[18px] pt-[9px]'>Let us handle the rest.</h3>
          <p className='text-white font-light sm:text-[20px] max-w-[582px] pt-[17px]'>Keep your reps “in the air” -- out in the field and on the phone where they can build relationships.</p>
 
-         <div className='max-w-[600px] flex flex-wrap gap-[32px] md:pt-[94px] sm:pt-20 pt-10'>
+         <div className='max-w-[700px] flex flex-wrap gap-[32px] md:pt-[94px] sm:pt-20 pt-10'>
            {RestData.map(({percent,des},index)=>(
              <div key={index} className='max-w-[194px]'>
                <p className='text-Aqua font-Montserrat font-bold md:text-[40px] sm:text-[34px] text-[25px]'>{percent}</p>
